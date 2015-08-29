9355fd790fed91beb29037408b4a20ed11cd33ff
Piotrek <piotr.kieltyka@pjwstk.edu.pl>
2015-08-29 14:21:06 +0200

    commit

---
+++ src/Participant.java
@@ -0,0 +1,16 @@
+public class Participant {
+    String name;
+    String surname;
+    String email;
+    String company;
+    String origin;
+    String position;
+
+    Participant (String name, String surname, String email, String company, String origin, String position) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.origin = origin;
+        this.position = position;
+    }
+}
---
+++ src/Zad1.java
@@ -0,0 +1,64 @@
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/Zad1")
+
+public class Zad1 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public Zad1() {
+        super();
+    }
+
+    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletContext context = request.getServletContext();
+        Integer counter = (Integer)context.getAttribute("counter");
+        counter = counter != null ? counter : 0;
+
+        int maxParticipants = 5;
+
+        if (counter == maxParticipants) {
+            response.sendRedirect("LimitReached.jsp");
+        }
+        else
+        {
+            HttpSession session = request.getSession();
+
+            if (session.getAttribute("state") != null) {
+                response.sendRedirect("AlreadyRegistered.jsp");
+            }
+            else
+            {
+                String state = "registered";
+                session.setAttribute("state", state);
+
+                String name = request.getParameter("name");
+                String surname = request.getParameter("surname");
+                String email = request.getParameter("email");
+                String company = request.getParameter("company");
+                String origin = request.getParameter("origin");
+                String position = request.getParameter("position");
+
+                Participant participant = new Participant(name, surname, email, company, origin, position);
+
+                counter++;
+                context.setAttribute("counter", counter);
+
+                response.sendRedirect("Registered.jsp");
+            }
+        }
+    }
+}
